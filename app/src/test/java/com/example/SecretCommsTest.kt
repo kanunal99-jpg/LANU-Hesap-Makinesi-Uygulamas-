ac285951ca85b2fe0fa8cd34cb1466ef4d05416a
kanunal99-jpg <kanunal99@gmail.com>
2026-09-18 16:58:54 +0300

    test: correct Robolectric SDK annotations

--- app/src/test/java/com/example/SecretCommsTest.kt
+++ app/src/test/java/com/example/SecretCommsTest.kt
@@ -15,11 +15,9 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
-import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [35])
-@Config(sdk = [36])
 class SecretCommsTest {
 
     @get:Rule
