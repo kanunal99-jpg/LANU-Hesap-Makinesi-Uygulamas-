6c3ce8c8e2ffe75b0505f543e7ec2bcb33c60bcf
kanunal99-jpg <kanunal99@gmail.com>
2026-09-18 16:58:35 +0300

    test: pin Robolectric tests to supported SDK 35

--- app/src/test/java/com/example/GreetingScreenshotTest.kt
+++ app/src/test/java/com/example/GreetingScreenshotTest.kt
@@ -13,6 +13,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
+
+@Config(sdk = [35])
 import org.robolectric.annotation.GraphicsMode
 
 @RunWith(RobolectricTestRunner::class)
