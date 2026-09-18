c4658ab0bbeb76d770fdfc134b7fbfcf84be5a09
kanunal99-jpg <kanunal99@gmail.com>
2026-09-18 16:58:51 +0300

    test: correct Robolectric SDK annotations

--- app/src/test/java/com/example/GreetingScreenshotTest.kt
+++ app/src/test/java/com/example/GreetingScreenshotTest.kt
@@ -14,7 +14,6 @@ import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
-@Config(sdk = [35])
 import org.robolectric.annotation.GraphicsMode
 
 @RunWith(RobolectricTestRunner::class)
